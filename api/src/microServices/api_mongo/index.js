7aa5d60e1ecf8bc1f3a99b3ccc6be88d8c64a5b6
Ramiro <ramiroignacio_14@hotmail.com>
2023-12-08 02:27:47 -0300

    microservice 8/12

--- api/src/microServices/api_mongo/index.js
+++ api/src/microServices/api_mongo/index.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const { daily_survey_post, testRequest } = require("./endpoints");
 const microServices = {};
 
@@ -52,21 +53,71 @@ microServices.createRetro = async (
   c3 = [],
   c4 = []
 ) => {
-  daily_survey_post(body);
+  const body = {
+    team_id,
+    sprint,
+    c1,
+    c2,
+    c3,
+    c4
+  }
+const sendRequest = await  daily_survey_post(body);
+return sendRequest;
 };
 
 //* Services to get data from mongo.
 
-microServices.getAnalytics = async (body) => {
-  daily_survey_post(body);
-};
+microServices.getAnalytics = async (                      
+  daily_survey = 0,
+  date = 0,
+  self_satisfaction = 15,
+  survey = 0,
+  user_id= "",
+  sprint = 1,
+  question1 = 5,
+  question2 = 5,
+  question3 = 6,
+  question4 = 6,
+  comment = "good team",                   
+  team_collaboration = 12,
+  work_engagement= 10,
+  workspace= 8    
+) => {
+  const query = {
+    daily_survey,
+    date,
+    self_satisfaction,
+    survey,
+    user_id,
+    sprint,
+    question1,
+    question2,
+    question3,
+    question4,
+    comment,                   
+    team_collaboration,
+    work_engagement,
+    workspace   
+  };
+  const sendRequest = await daily_survey_post(query);
+  return sendRequest;
+} 
 
-microServices.getReports = async (body) => {
-  daily_survey_post(body);
-};
 
-microServices.getAnalyticsTimeline = async (body) => {
-  daily_survey_post(body);
-};
+microServices.getReports = async ( 
+  user_id="text",
+  comment="text"
+) =>{
+  const body = {
+    user_id,
+    comment
+  };
+  const sendRequest = await daily_survey_post(body);
+  return sendRequest;
+}
+
+microServices.getAnalyticsTimeline = async () => {
+
+} 
 
 module.exports = microServices;
