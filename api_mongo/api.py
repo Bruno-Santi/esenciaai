2d0fbe3af910f5017379225256f04bf1501e603d
lilJuan4168 <chesteraraoz12.0@gmail.com>
2023-12-09 21:35:13 -0300

    api_mongo update

--- api_mongo/api.py
+++ api_mongo/api.py
@@ -22,37 +22,57 @@ db = client["essenciaIA_app"]
 
 #post methods -----------------------
 
+@app.get("/test")
+def testing(user_id, team_id):
+      current_date = str(date.today())
+      daily_check = json_util.dumps(db["survey_data"].find_one({
+                  "team_id": team_id,
+                  f"daily_survey.{current_date}.survey":{"$elemMatch":{"user_id":user_id}}
+                  }))
+      if daily_check is "null":
+            return f"ok, {daily_check}"
+      else:
+            return f"fail, {daily_check}"
+      #return json.loads(daily_check)
+
 @app.post("/daily_survey")
 async def add_daily_survey(survey:dailySurvey):
         current_date = str(date.today())
         #current_date = "2023-12-07"
         try:
-            db["survey_data"].update_one(filter={
+            daily_check = json_util.dumps(db["survey_data"].find_one({
+                "team_id": survey.team_id,
+                f"daily_survey.{current_date}.survey":{"$elemMatch":{"user_id":survey.user_id}}
+                }))
+            if daily_check is "null":   
+                db["survey_data"].update_one(filter={
                 "team_id": survey.team_id
-            }, update={
-      "$push":{
-            f"daily_survey.{current_date}.survey": {
-             "user_id": survey.user_id,
-             "sprint": survey.sprint,
-             "question1": survey.question1,
-             "question2": survey.question2,
-             "question3": survey.question3,
-             "question4": survey.question4,
-             "comment": {"content": survey.comment}}
-              }, 
-      "$inc":{"daily_survey_count": 1, 
-              "self_satisfaction_general": survey.question1,
-              "work_engagement_general": survey.question2,
-              "team_collaboration_general": survey.question3,
-              "workspace_general": survey.question4,
-              f"daily_survey.{current_date}.self_satisfaction": survey.question1,
-              f"daily_survey.{current_date}.work_engagement": survey.question2,
-              f"daily_survey.{current_date}.team_collaboration": survey.question3,
-              f"daily_survey.{current_date}.workspace": survey.question4
-              },
-      "$set":{"retro_count":0, "reports_count":0}
-    }, upsert=True)
-            return {"status":200}
+                }, update={
+                "$push":{
+                f"daily_survey.{current_date}.survey": {
+                "user_id": survey.user_id,
+                "sprint": survey.sprint,
+                "question1": survey.question1,
+                "question2": survey.question2,
+                "question3": survey.question3,
+                "question4": survey.question4,
+                "comment": {"content": survey.comment}}
+                }, 
+                "$inc":{"daily_survey_count": 1, 
+                "self_satisfaction_general": survey.question1,
+                "work_engagement_general": survey.question2,
+                "team_collaboration_general": survey.question3,
+                "workspace_general": survey.question4,
+                f"daily_survey.{current_date}.self_satisfaction": survey.question1,
+                f"daily_survey.{current_date}.work_engagement": survey.question2,
+                f"daily_survey.{current_date}.team_collaboration": survey.question3,
+                f"daily_survey.{current_date}.workspace": survey.question4
+                },
+                "$set":{"retro_count":0, "reports_count":0}
+                }, upsert=True)
+                return {"status":200}
+            else:
+                return {"status":400, "msg": "user already completed the survey"}
         except Exception as e:
             return {"status":422, "error":e }
 
