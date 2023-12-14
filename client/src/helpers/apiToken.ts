23e95c5074af3649de4cb875dae6c6634ce8ee95
Bruno Santimaria <55465805+Bruno-Santi@users.noreply.github.com>
2023-12-14 04:42:08 -0300

    Update apiToken.ts

--- client/src/helpers/apiToken.ts
+++ client/src/helpers/apiToken.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://esencia-api-1b5d609d081b.herokuapp.com",
 });
 
 api.interceptors.request.use(
