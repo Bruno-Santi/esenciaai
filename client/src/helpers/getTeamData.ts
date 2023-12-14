e8d6b675adbec773b99687bb7429917027349cce
Bruno Santimaria <55465805+Bruno-Santi@users.noreply.github.com>
2023-12-14 11:13:44 -0300

    Update getTeamData.ts

--- client/src/helpers/getTeamData.ts
+++ client/src/helpers/getTeamData.ts
@@ -1,7 +1,7 @@
 import api from "./apiToken";
 
 export const getTeamData = async (teamId: string) => {
-  const response = await api.get(`/auth/${teamId}`);
+  const response = await api.get(`https://us-central1-esencia-app.cloudfunctions.net/dashboard_data?team_id=${teamId}`);
   console.log(response)
   return response.data;
 };
