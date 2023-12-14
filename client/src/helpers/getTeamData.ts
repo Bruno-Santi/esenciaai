5b5ffbded291d08e81d77ca2380905d083293b27
Bruno Santimaria <55465805+Bruno-Santi@users.noreply.github.com>
2023-12-14 11:16:04 -0300

    Update getTeamData.ts

--- client/src/helpers/getTeamData.ts
+++ client/src/helpers/getTeamData.ts
@@ -1,7 +1,7 @@
 import api from "./apiToken";
-
+import "axios" from "axios"
 export const getTeamData = async (teamId: string) => {
-  const response = await api.get(`https://us-central1-esencia-app.cloudfunctions.net/dashboard_data?team_id=${teamId}`);
+  const response = await axios.get(`https://us-central1-esencia-app.cloudfunctions.net/dashboard_data?team_id=${teamId}`);
   console.log(response)
   return response.data;
 };
