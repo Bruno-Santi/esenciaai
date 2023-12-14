0bd72fa08b8eb2f472fe74585026de0f4837a00b
Bruno Santimaria <55465805+Bruno-Santi@users.noreply.github.com>
2023-12-14 11:17:17 -0300

    Update getTeamData.ts

--- client/src/helpers/getTeamData.ts
+++ client/src/helpers/getTeamData.ts
@@ -1,7 +1,13 @@
 import api from "./apiToken";
 import "axios" from "axios"
 export const getTeamData = async (teamId: string) => {
-  const response = await axios.get(`https://us-central1-esencia-app.cloudfunctions.net/dashboard_data?team_id=${teamId}`);
+  const response = await axios.get(`https://us-central1-esencia-app.cloudfunctions.net/dashboard_data?team_id=${teamId}`, {
+      headers: {
+        'Access-Control-Allow-Origin': 'https://esenciaai.vercel.app',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      },
+    });
   console.log(response)
   return response.data;
 };
