c6f052f1cbec050baad3dcc2fe6ebc2c707badd5
Bruno Santimaria <55465805+Bruno-Santi@users.noreply.github.com>
2023-12-14 11:10:29 -0300

    Update getTeamData.ts

--- client/src/helpers/getTeamData.ts
+++ client/src/helpers/getTeamData.ts
@@ -2,5 +2,6 @@ import api from "./apiToken";
 
 export const getTeamData = async (teamId: string) => {
   const response = await api.get(`/auth/${teamId}`);
+  console.log(response)
   return response.data;
 };
