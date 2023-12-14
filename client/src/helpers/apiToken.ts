c57e2cce45a2cd7200b662c11f2d78f18396025f
Bruno Santimaria <55465805+Bruno-Santi@users.noreply.github.com>
2023-12-14 11:00:15 -0300

    Update apiToken.ts

--- client/src/helpers/apiToken.ts
+++ client/src/helpers/apiToken.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://esencia-api-vercel-production.up.railway.app",
+  baseURL: "https://9qhvw5j9-3000.brs.devtunnels.ms",
 });
 
 api.interceptors.request.use(
