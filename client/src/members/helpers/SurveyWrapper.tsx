88c9e538282a8801eb79fc86c242306f998bb43f
Bruno Santimaria <55465805+Bruno-Santi@users.noreply.github.com>
2023-12-13 22:21:43 -0300

    Update SurveyWrapper.tsx

--- client/src/members/helpers/SurveyWrapper.tsx
+++ client/src/members/helpers/SurveyWrapper.tsx
@@ -8,25 +8,29 @@ const SurveyWrapper = () => {
   const navigate = useNavigate();
   const location = useLocation();
  const searchParams = new URLSearchParams(location.search);
+
+ const [params,setParams] = useState({token:"",team_id:""});
+
   const token = searchParams.get("token");
   const team_id = searchParams.get("team_id");
-  console.log(token)
-   const setTokenInLocalStorage = () => {
-      if (token) {
-        localStorage.setItem("authToken", token);
-      }
-    };
 
-    setTokenInLocalStorage();
-  useEffect(() => {
-   if (token && team_id) {
-    <Survey team_id={team_id} token={token}/>
-  }
-  }, []);
+  //  const setTokenInLocalStorage = () => {
+  //     if (token) {
+  //       localStorage.setItem("authToken", token);
+  //     }
+  //   };
 
+useEffect(() => {
+setParams({token:searchParams.get("token"),team_id:searchParams.get("team_id")});
+}, [searchParams]);
 
+// setTokenInLocalStorage();
 
-  return null;
+  
+  if (params && params.team_id && params.token) {
+   return (<Survey team_id={params.team_id} token={params.token}/>)
+ }
+else return null;
 };
 
 export default SurveyWrapper;
