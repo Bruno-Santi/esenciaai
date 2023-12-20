dc41c66ba8a33f89e5aeff265be569311efc3b4d
Bruno Santimaria <55465805+Bruno-Santi@users.noreply.github.com>
2023-12-20 15:39:25 -0300

    Update apiToken.ts

--- client/src/helpers/apiToken.ts
+++ client/src/helpers/apiToken.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //% FACU_EDIT
 
-export const baseURL = "https://9qhvw5j9-3000.brs.devtunnels.ms";
+export const baseURL = "https://esencia-api-1b5d609d081b.herokuapp.com";
 // https://9qhvw5j9-3000.brs.devtunnels.ms
 //% END
 
