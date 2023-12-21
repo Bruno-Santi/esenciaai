2f0d5c400b14dfcabd6ab89d7b3040af2278ac05
Facundo Alvarez <95634052+finstory@users.noreply.github.com>
2023-12-21 03:42:28 -0300

    Update apiToken.ts

--- client/src/helpers/apiToken.ts
+++ client/src/helpers/apiToken.ts
@@ -2,8 +2,9 @@ import axios from "axios";
 
 //% FACU_EDIT
 
-export const baseURL = "https://esencia-api-1b5d609d081b.herokuapp.com";
-// https://9qhvw5j9-3000.brs.devtunnels.ms
+export const baseURL = 
+"https://esencia-api-1b5d609d081b.herokuapp.com";
+// "http://localhost:3000";
 //% END
 
 const api = axios.create({
