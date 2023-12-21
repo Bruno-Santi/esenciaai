98531a730a17121cc545d828824dd0ed9ee49c73
Bruno Santimaria <brun0.23@outlook.com>
2023-12-21 11:18:14 -0300

    fix

--- client/src/layaout/SurveyLayout.tsx
+++ client/src/layaout/SurveyLayout.tsx
@@ -11,10 +11,10 @@ export const SurveyLayout: React.FC<LayoutProps> = ({ children }) => {
         alt='esencia.ai logo'
       />
       <div className='flex flex-col'>
-        <h4 className='text-tertiary font-normal tracking-tighter flex mt-20 text-5xl font-poppins mx-auto'>
+        <h4 className='text-tertiary font-normal tracking-tighter flex mt-20 md:text-2xl lg:text-5xl font-poppins mx-auto'>
           Welcome!
         </h4>
-        <p className='text-center font-poppins font-extralight md:text-lg lg:text-xl w-2/3 justify-center mx-auto mt-6 text-tertiary'>
+        <p className='text-center font-poppins font-extralight md:text-base lg:text-xl w-2/3 justify-center mx-auto mt-6 text-tertiary'>
           You’ve been invited to fulfill a <span className='text-secondary font-normal'>Esencia</span> survey. This is
           the main tool through which data is gathered We really appreciate you{" "}
           <span className='text-secondary font-normal'>taking the time to answer</span>, as this will be the main input
